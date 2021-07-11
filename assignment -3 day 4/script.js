abb1fcc94894110b7d9c5e3148ac4c75465af664
Piyush Jaiswal <68432611+Piyush-jaiswal-pj@users.noreply.github.com>
2021-07-12 01:16:04 +0530

    assignment 3 day 4

--- assignment -3 day 4/script.js
+++ assignment -3 day 4/script.js
@@ -1,5 +1,5 @@
 function getdata() {
     var Name = document.getElementById('name');
     var Value = Name.value;
-    document.getElementById('msg').innerText = " Welcome " + Value + " ... ";
+    document.getElementById('text').innerText = " Welcome " + Value + " ... ";
 }
